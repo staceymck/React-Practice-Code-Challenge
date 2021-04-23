6944652c5b5f466ba8be4e9dc3aed1f9ab15397c
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-23 12:20:18 -0500

    commit tree 3a6f28641ef9acc1cfe6d720da47ef9fb6e7ff4c

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,79 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import SushiWallet from './components/SushiWallet'
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    eaten: [],
+    sushiIndex: 0,
+    money: 100
+  }
+
+  pullFromWallet = (amt) => {
+    this.setState(state => {
+      return{
+        money: state.money + parseInt(amt)
+      }
+    })
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        sushi: json
+      })
+    })
+  }
+
+  pay = (sushi) => {
+    if(this.state.money - sushi.price >= 0) {
+      this.setState(state => {
+        return {
+          money: state.money - sushi.price
+        }
+      })
+    }
+  }
+
+  eat = (sushi) => {
+    if(this.state.money - sushi.price >= 0) {
+      this.setState(state => {
+        return {
+          money: state.money - sushi.price,
+          eaten: [...state.eaten, sushi]
+        }
+      })
+    } else {
+      alert("Sorry, not enough money!")
+    }
+  }
+  
+  advance = () => {
+    this.setState(state => {
+      return {
+        sushiIndex: state.sushiIndex + 4
+      }
+    })
+  }
+
+  displayWallet = () => {
+
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushiIndex={this.state.sushiIndex} advance={this.advance} sushi={this.state.sushi} eaten={this.state.eaten} eat={this.eat} />
+        <Table eaten={this.state.eaten} money={this.state.money}/>
+        <SushiWallet pullFromWallet={this.pullFromWallet} />
       </div>
     );
   }
